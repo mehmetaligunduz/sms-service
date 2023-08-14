6db7ee448cc4aa8143a69059af700bc761b3f061
mehmetaligunduz <magndz@gmail.com>
2023-08-14 23:46:17 +0300

    rearranged constant variables

--- src/main/java/com/smsservice/listener/MessageListener.java
+++ src/main/java/com/smsservice/listener/MessageListener.java
@@ -16,16 +16,15 @@ public class MessageListener {
     private final WebClient.Builder webClientBuilder;
 
     private final SmsService smsService;
-
-
     private static final String QUEUE = "otp_code_sms_queue";
+    public static final String OTP_SERVICE_URL = "http://localhost:8080/otp-service/v1/";
 
     @RabbitListener(queues = QUEUE)
     public void listener(MessageEntity message) {
 
         String code = webClientBuilder.build()
                 .get()
-                .uri("http://localhost:8080/otp-service/v1/" + message.getMessage())
+                .uri(OTP_SERVICE_URL + message.getMessage())
                 .retrieve()
                 .bodyToMono(String.class)
                 .block();
@@ -34,7 +33,6 @@ public class MessageListener {
             return;
 
         smsService.send(message.getTo(), code);
-
 
     }
 
